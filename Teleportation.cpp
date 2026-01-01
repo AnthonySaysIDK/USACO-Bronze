4ff47942280ea1ff73fd15c67192d9255988dba5
Anthony <anthonyzhfu@gmail.com>
2026-01-01 07:53:58 -0500

    Update solutions

---
+++ Teleportation.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int start, end, x, y;
+    cin >> start >> end >> x >> y;
+    cout << start;
+    return 0;
+}
+/*
+Compile & Run:
+g++ -std=gnu++17 -O2 -Wall FILENAME.cpp -o FILENAME
+./FILENAME
+
+add input and output text:
+./Template < input.txt > output.txt
+
+*/
